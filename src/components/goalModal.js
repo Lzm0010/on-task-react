8e60b8c93994b8d0954c21e2a69aaaf5d75cefd0
Lee McCormick <lzm0010@gmail.com>
2020-05-27 16:38:02 -0500

    finished goal form

--- src/components/goalModal.js
+++ src/components/goalModal.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useInput} from '../hooks/useInput';
-import {Form, Modal, Select} from 'semantic-ui-react';
+import {useSelect} from '../hooks/useSelect';
+import DatePicker from 'react-datepicker';
+import {Form, Modal, Select, Button, Divider, Input} from 'semantic-ui-react';
 
 
 const GoalModal = ({modalOpen, handleClose}) => {
     const {value:name, bind:bindName, reset:resetName} = useInput("");
-    const {value:goalType, bind:bindGoalType, reset:resetGoalType} = useInput("");
-    const {value:frequency, bind:bindFrequency, reset:resetFrequency} = useInput("");
+    const {value:goalType, bind:bindGoalType, reset:resetGoalType} = useSelect("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(null);
     const {value:goalTotalDays, bind:bindGoalTotalDays, reset:resetGoalTotalDays} = useInput("");
+    const {value:goalPercentage, bind:bindGoalPercentage, reset:resetGoalPercentage} = useInput("");
+    const {value:frequency, bind:bindFrequency, reset:resetFrequency} = useSelect("");
 
     const typeOptions = [
         {key:'total', value:'total', text: 'Total'},
         {key:'percentage', value:'percentage', text: 'Percentage'},
     ];
+
     const freqOptions = [
         {key:'daily', value:'daily', text: 'Daily'},
         {key:'eoday', value:'eoday', text: 'Every other day'},
@@ -21,18 +27,45 @@ const GoalModal = ({modalOpen, handleClose}) => {
         {key:'monthly', value:'monthly', text: 'Monthly'},
         {key:'bimonthly', value:'bimonthly', text: 'Bi-Monthly'}
     ];
-    // t.string "name"
-    // t.datetime "start_date"
-    // t.datetime "end_date"
-    // t.string "goal_type"
-        //Total - description
-        //Percentage - description
-    // t.integer "goal_total_days" only if total
-    // t.float "goal_percentage" only if percentage
-    // t.bigint "user_id", null: false
-    // t.string "frequency"
-        //daily, weekly, biweekly, monthly, bimothly
 
+    const newGoal = () => {
+        const goalUrl = `http://localhost:3000/goals`;
+        const goalObj = {
+            'method': 'POST',
+            'headers': {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }, 
+            'body': JSON.stringify({
+                name, 
+                goal_type: goalType,
+                start_date: startDate,
+                end_date: endDate,
+                goal_total_days: goalTotalDays,
+                goal_percentage: goalPercentage,
+                frequency,
+                user_id: 1
+            })
+        };
+        fetch(goalUrl, goalObj)
+        .then(res => res.json())
+        .then(goal => {
+            // addTask(task)
+            console.log(goal);
+        })
+    }
+    
+    const handleSubmit = () => {
+        newGoal();
+        handleClose();
+        resetName();
+        resetGoalType();
+        resetGoalTotalDays();
+        resetGoalPercentage();
+        resetFrequency();
+        setStartDate(new Date());
+        setEndDate(null);
+    };
 
     return(
         <Modal
@@ -42,11 +75,71 @@ const GoalModal = ({modalOpen, handleClose}) => {
         >
             <Modal.Header>Add a Goal</Modal.Header>
             <Modal.Content>
-                <Form>
-                    <Form.Input label="Goal" placeholder="Enter the goal you want to achieve..." {...bindName}/>
-                    <Select placeholder="Goal Type" options={typeOptions} {...bindGoalType}/>
-                    <Select placeholder="Frequency" options={freqOptions} {...bindFrequency} />
-                    {(goalType === "total" ? <Form.Input label="Total" placeholder="Total number of days you want to complete the goal.." {...bindGoalTotalDays} /> : null)}
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group widths="equal">
+
+                        <Form.Field
+                            control={Input}
+                            label="Goal" 
+                            placeholder="Enter the goal you want to achieve..." 
+                            {...bindName}
+                        />
+
+                        <Form.Field 
+                            control={Select}
+                            label="Goal Type"
+                            placeholder="This is the type of goal that will be displayed on the calendar!" 
+                            options={typeOptions} 
+                            {...bindGoalType}
+                        />
+
+                    </Form.Group>
+
+                    <Form.Group widths='equal'>
+
+                        <Form.Field>
+                            <label>Start Date</label>
+                            <DatePicker 
+                                selected={startDate}
+                                onChange={date => setStartDate(date)}
+                                selectsStart
+                                startDate={startDate}
+                                endDate={endDate}
+                            />
+                        </Form.Field>
+
+                        <Form.Field>
+                            <label>End Date</label>
+                            <DatePicker 
+                                selected={endDate}
+                                onChange={date => setEndDate(date)}
+                                selectsEnd
+                                startDate={startDate}
+                                endDate={endDate}
+                                minDate={startDate}
+                                placeholderText="Select end date.."
+                            />
+                        </Form.Field>
+
+                    </Form.Group>
+
+                    <Form.Group widths="equal">
+
+                        {(goalType === "total" ? <Form.Input fluid label="Total" placeholder="Total number of days you want to complete the goal.." {...bindGoalTotalDays} /> : null)}
+                        {(goalType === "percentage" ? <Form.Input fluid label="Percentage" placeholder="Percentage of days to complete over this time span.." {...bindGoalPercentage} /> : null)}
+                        <Form.Field
+                            control={Select}
+                            label="Frequency"
+                            placeholder="Select how often you want to accomplish this goal.." 
+                            options={freqOptions} 
+                            {...bindFrequency}
+                        />
+
+                    </Form.Group>
+
+                    <Divider hidden/>
+
+                    <Button type='submit'>Submit</Button>
                 </Form>
             </Modal.Content>
         </Modal>
