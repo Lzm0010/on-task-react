17dda14967b9e6435eabff9fb1cb129034cda5e5
github username <github email address>
2020-06-01 09:56:35 -0500

    added project modal

--- src/containers/calendarContainer.js
+++ src/containers/calendarContainer.js
@@ -1,6 +1,4 @@
 import React, {Fragment} from 'react';
-import GoalContainer from './goalContainer';
-import ProjectContainer from './projectContainer';
 import Calendar from '../components/calendar/calendar';
 
 const CalendarContainer = ({showDay, tasks}) => {
