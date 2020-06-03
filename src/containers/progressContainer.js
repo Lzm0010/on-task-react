83d7b92265dd8d89071a4cd5dbfe9c183b765690
github username <github email address>
2020-06-03 17:37:55 -0500

    added delete for projects and associated tasks

--- src/containers/progressContainer.js
+++ src/containers/progressContainer.js
@@ -1,16 +1,30 @@
 import React, {Fragment} from 'react';
 import Goal from '../components/goal';
 import Project from '../components/project'
-import {Divider, Icon, Header} from 'semantic-ui-react';
+import {Divider, Icon, Header, Card} from 'semantic-ui-react';
 
-const ProgressContainer = ({goals, projects}) => {
+const ProgressContainer = ({goals, projects, removeGoal, removeProject, removeTask}) => {
+
+    // const deleteTask = (task) => {
+    //     const taskUrl = `http://localhost:3000/tasks/${task.id}`
+    //     const taskObj = {
+    //         'method': 'DELETE',
+    //         'headers': {
+    //             "Accept": "application/json",
+    //             "Content-Type": "application/json"
+    //         }
+    //     }
+    //     fetch(taskUrl, taskObj)
+    //     .then(res => res.json())
+    //     .then(task => removeTask(task))
+    // }
 
     const displayProgressGoals = () => (
-        goals.map(goal => <Goal key={`g-${goal.id}`} goal={goal}/>)
+        goals.map(goal => <Goal key={`g-${goal.id}`} goal={goal} removeGoal={removeGoal} removeTask={removeTask}/>)
     )
     
     const displayProgressProjects = () => (
-        projects.map(project => <Project key={`p-${project.id}`} project={project} />)
+        projects.map(project => <Project key={`p-${project.id}`} project={project} removeProject={removeProject} removeTask={removeTask}/>)
     )
 
     return (
@@ -21,14 +35,18 @@ const ProgressContainer = ({goals, projects}) => {
                     Goals
                 </Header>
             </Divider>
-            {displayProgressGoals()}
+            <Card.Group>
+                {displayProgressGoals()}
+            </Card.Group>
             <Divider horizontal>
                 <Header as='h4'>
                     <Icon name='road'/>
                     Projects
                 </Header>
             </Divider>
-            {displayProgressProjects()}
+            <Card.Group>
+                {displayProgressProjects()}
+            </Card.Group>
         </Fragment>
     )
 }
